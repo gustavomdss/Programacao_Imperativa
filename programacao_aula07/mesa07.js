d5eaa699224fd11341c5adaca8572079ec2848fe
gustavomdss <gustavo.dourados@gmail.com>
2022-08-15 21:09:48 -0400

    mesa07

--- programacao_aula07/mesa07.js
+++ programacao_aula07/mesa07.js
@@ -1,16 +1,40 @@
 // ----------------EXERCICIO 01-------------------//
 // 1- false
+console.log(!true);
+
 // 2- true
-// 3- erro
+console.log(!false);
+
+// 3- error
+//console.log(!!false true);
+
 // 4- true
+console.log(!!true);
+
 // 5 - false
+console.log(!1);
+
 // 6- true
+console.log(!!1);
+
 // 7- true
+console.log(!0);
+
 // 8- false
+console.log(!!0);
+
 // 9- false
+console.log(!!"");
 // 10- a: false
 //      b: true
 //      c: true
+
+let x = 5 ;
+let y = 9;
+console.log (x < 10 && x!==5);
+console.log (x>9 || x===5);
+console.log (!(x===y));
+
 
 //------------------EXERCICIO 02------------------------//
 
