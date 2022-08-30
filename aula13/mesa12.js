adcc69ca0113e92b51c9037c221b6ee8770b7353
gustavomdss <gustavo.dourados@gmail.com>
2022-08-29 22:10:09 -0400

    1commit

---
+++ aula13/mesa12.js
@@ -0,0 +1,23 @@
+
+
+const conta = {
+    titular : 'fulano',
+    numeroConta : 125566,
+    tipo : 'corrente',
+    saldo : 1500.00,
+
+}
+
+const dados = [{"titular": 
+
+
+}]
+
+function Conta(titular, numeroConta, tipo, saldo){
+    this.titular = titular;
+    this.numero = numeroConta;
+    this.tipo = tipo;
+    this.saldo = saldo;
+}
+let conta1 = new Conta(dados[0].titular, 455514, 'corrente', 1500.00)
+console.log(conta1)
