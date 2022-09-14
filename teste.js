3962503919f83f65eb555c1b92e1122e75164282
gustavomdss <gustavo.dourados@gmail.com>
2022-09-13 20:16:35 -0400

    commit

--- teste.js
+++ teste.js
@@ -1,36 +1,4 @@
-const dados = [
-  {
-      "titular": "Abigael Natte",
-      "numero": 5486273622,
-      "tipo": "Conta Corrente",
-      "saldo": 27771
-  },
-  {
-      "titular": "Ramon Connell",
-      "numero": 1183971869,
-      "tipo": "Conta Corrente",
-      "saldo": 8675
-  },
-  {
-      "titular": "Fulano",
-      "numero": 1183971869,
-      "tipo": "Conta Corrente",
-      "saldo": 8675
-  }
-
-]
-
-function Conta(titular, numero, tipo, saldo) {
-  this.titular = titular;
-  this.numero = numero;
-  this.tipo = tipo;
-  this.saldo = saldo;
+for (let index = 0; index < 50; index+=10) {
+  console.log(index)
 }
-
-// let conta1 = new Conta(dados[0].titular, dados[0].numero, dados[0].tipo, dados[0].saldo);
-// let conta2 = new Conta(dados[1].titular, dados[1].numero, dados[1].tipo, dados[1].saldo);
-let listaContas = [];
-for (let contador = 0; contador < dados.length; contador++) {
-  listaContas.push(new Conta(dados[contador].titular, dados[contador].numero, dados[contador].tipo, dados[contador].saldo) );
-}
-console.log(listaContas);
+console.log(index)
