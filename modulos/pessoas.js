4a391e81b73d2e357c39fc10a6746bca15e45755
gustavomdss <gustavo.dourados@gmail.com>
2022-09-19 21:48:21 -0400

    segunda_aplicacao

---
+++ modulos/pessoas.js
@@ -0,0 +1,63 @@
+let pessoas = [
+    {
+        altura : 1.80,
+        sexo : "M"
+    },
+    {
+        altura : 1.55,
+        sexo : "F"
+    },
+    {
+        altura : 1.65,
+        sexo : "F"
+    },
+    {
+        altura : 1.87,
+        sexo : "M"
+    },
+    {
+        altura : 1.68,
+        sexo : "M"
+    },
+    {
+        altura : 1.50,
+        sexo : "F"
+    },
+    {
+        altura : 1.64,
+        sexo : "F"
+    },
+    {
+        altura : 1.79,
+        sexo : "M"
+    },
+    {
+        altura : 1.59,
+        sexo : "F"
+    },
+    {
+        altura : 1.61,
+        sexo : "M"
+    },
+    {
+        altura : 1.72,
+        sexo : "M"
+    },
+    {
+        altura : 1.84,
+        sexo : "M"
+    },
+    {
+        altura : 1.69,
+        sexo : "F"
+    },
+    {
+        altura : 1.63,
+        sexo : "M"
+    },
+    {
+        altura : 1.54,
+        sexo : "F"
+    }
+]
+module.exports = pessoas;
